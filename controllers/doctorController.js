159746b91c17cf3d438100802fb58c6b3a7970d5
TheWebAlok <alokprakash.net@gmail.com>
2026-09-15 10:50:42 +0530

    Fix upload middleware filename

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -1,5 +1,5 @@
 const Doctor = require("../models/Doctor.js");
-const { deletePhotoFile } = require("../middleware/upload.js");
+const { deletePhotoFile } = require("../middleware/Upload.js");
 
 // =====================================================
 // ADD DOCTOR - ADMIN ONLY
